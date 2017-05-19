66cf48e610b1fe7cae03cb37ebbaba8800f2e5b0
ClemZor <c.carolle.c@gmail.com>
2017-05-19 15:08:52 +0200

    clem: modif bean project

--- src/main/java/fr/acdo/domain/Project.java
+++ src/main/java/fr/acdo/domain/Project.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -24,4 +26,7 @@ public class Project {
 	@Size(min = 2, max = 45, message = "Le nom doit faire au moins 2 caractères")
 	private String name;
 
+	@ManyToOne
+	@JoinColumn(name = "family_id")
+	private Family family;
 }
